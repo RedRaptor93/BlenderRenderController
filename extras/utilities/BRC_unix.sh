1fd185e3f522c0ead4938d133f887422e8f03c28
Pedro Oliva Rodrigues <pedroolivarodrigues@outlook.com>
2018-10-26 12:47:58 -0300

    updated utilities

--- extras/utilities/BRC_unix.sh
+++ extras/utilities/BRC_unix.sh
@@ -1,6 +1,3 @@
 #!/bin/sh
 
-# Script to run BRC on Unix based systems	-	21/03/2017
-# tested on Debian 8.7 x64
-
-exec mono "./BlenderRenderController.exe" "$@"
+dotnet run "./BlenderRenderController.dll" "$@"
